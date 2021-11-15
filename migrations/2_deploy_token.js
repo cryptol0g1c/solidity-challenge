351fae42feb4692b3d8b53659b28654a60089db0
yyb930803 <54490903+yyb930803@users.noreply.github.com>
2021-11-15 14:28:05 +0800

    del mig

--- migrations/2_deploy_token.js
+++
@@ -1,5 +0,0 @@
-const Mt0Token = artifacts.require("Mt0Token");
-
-module.exports = function (deployer) {
-  deployer.deploy(Mt0Token);
-};
--- migrations/3_deploy_token.js
+++
@@ -1,5 +0,0 @@
-const Mt1Token = artifacts.require("Mt1Token");
-
-module.exports = function (deployer) {
-  deployer.deploy(Mt1Token);
-};
