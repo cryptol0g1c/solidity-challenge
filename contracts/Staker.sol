2c94aa0ce80d6e9d450a3c5d1ab0af2a0a348079
leanonchain <leandroezeleguizamon@gmail.com>
2022-06-08 17:46:12 -0300

    Staker comment requirements

--- contracts/Staker.sol
+++ contracts/Staker.sol
@@ -1,8 +1,12 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "./RewardToken.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RewardToken is Ownable {
+/// @title ERC20 token used for staking/rewards in Staker contract
+/** @dev This contract will get deployed with some tokens minted for the distribution to the stakers. And then, according to a schedule, allocate the reward tokens to addresses that deposited those tokens into the contract. Then the allocated tokens are and divide by the total balance of the deposited tokens so each depositor get's proportional share of the rewards. Ultimately, a user will deposit some tokens and later will be able to withdraw the principal amount plus the earned rewards. The following functions must be implemented: deposit(), withdraw()
+ */
+contract Staker is Ownable {
     constructor() {}
 }
